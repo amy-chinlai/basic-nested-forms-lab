60f3816e42e69a1efa55baf680342d0b195863b2
amy-chinlai <amy@actionnetwork.org>
2021-11-03 10:23:09 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients 
   accepts_nested_attributes_for :ingredients
 
-  attr_accessor :ingredients_attributes #do I need this?
+  # attr_accessor :ingredients_attributes #do I need this?
 end
